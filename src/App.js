1d870274f18c6c140c27e20f7531027f66faad36
KarinaOlenina <ua.olenina@gmail.com>
2022-12-08 23:17:17 +0200

    create axios.js, store.js, posts.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import { Routes, Route } from "react-router-dom";
 import Container from "@mui/material/Container";
 
 import { Header } from "./components";
-import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import { AddPost, FullPost, Home, Login, Registration } from "./pages";
 
 function App() {
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-        <Home />
-        {/*<FullPost />*/}
-        {/*<AddPost />*/}
-        {/*<Login />*/}
-        {/*<Registration />*/}
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/posts/:id"} element={<FullPost />} />
+          <Route path={"/add-post"} element={<AddPost />} />
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/register"} element={<Registration />} />
+        </Routes>
       </Container>
     </>
   );
